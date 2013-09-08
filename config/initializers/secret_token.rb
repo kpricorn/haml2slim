3e3b176b3fd273095e9b4d1194ee88b4cdaa805c
Sebastian de Castelberg <sebu@kpricorn.org>
2013-09-08 20:15:56 +0200

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Haml2slim::Application.config.secret_key_base = 'a5b98b4a232b41b9a1c2a5d2b950e69aedad1e484f13d39ddfa5b9dbeb5d84e77756043d508417712833fe83e3e0b3fc3bd93f6a5a5fa3c329d1e4ada351a5b4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Haml2slim::Application.config.session_store :cookie_store, key: '_haml2slim_session'
