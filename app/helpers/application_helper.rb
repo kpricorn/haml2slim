f93e437818e17c6a9e4925aeadcb6b2b0fee9f82
Sebastian de Castelberg <sebu@kpricorn.org>
2013-09-24 10:51:50 +0200

    add meta tags

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  def keywords
+    [
+      'Haml',
+      'Slim',
+      'Conversion',
+      'Online Conversion',
+      'haml2slim',
+      'ruby',
+      'html',
+      'rails',
+      'ruby on rails',
+      'middleman'
+    ].join ', '
+  end
 end
